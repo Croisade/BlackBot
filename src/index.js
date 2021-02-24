fa23a41c537a60a5423a959dc7812f0bf4cadb84
Jamal Gardiner <jgardiner@neat.com>
2021-02-24 15:11:34 -0500

    feat: channel repost

--- src/index.js
+++ src/index.js
@@ -10,8 +10,9 @@ require('dotenv').config()
 const client = new Client()
 client.commands = new Discord.Collection()
 const embed = new Discord.MessageEmbed()
-
-const commandFiles = fs.readdirSync('./src/services').filter(file => file.endsWith('.js'))
+const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+const regex = new RegExp(expression)
+const commandFiles = fs.readdirSync('./src/services').filter((file) => file.endsWith('.js'))
 
 forEach(commandFiles, (file) => {
   const command = require(`./services/${file}`)
@@ -24,8 +25,13 @@ const prefix = '~'
 
 client.on('message', async (message) => {
   if (message.author.bot) return
-  if (!message.content.startsWith(prefix)) return
 
+  if (message.channel.id === '750807318249341159' && message.content.match(regex)) {
+    const channel = client.channels.cache.find((chan) => chan.id === '809473464020369419')
+    channel.send(message.content)
+  }
+
+  if (!message.content.startsWith(prefix)) return
   const args = message.content.substring(prefix.length).split(' ')
   const command = client.commands.get(args[0])
 
